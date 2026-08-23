dac5342b80c916977cadc4cf37444c10b5bbfffc
saranyanayudu <saranyanayudu@gmail.com>
2026-08-23 20:33:55 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,6 +1,2 @@
-﻿# Hardcoded API key (deliberate fail)
 # Hardcoded secret pattern for Gitleaks
-import os
-
-# Fetch secret safely from environment variables
-OPENAI_API_KEY = os.getenv("OPENAI_API_KEY", "sk-proj-1234567890abcdef1234567890abcdef")
+OPENAI_API_KEY = "sk-proj-1234567890abcdef1234567890abcdef"
